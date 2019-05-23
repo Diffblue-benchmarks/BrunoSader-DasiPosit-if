8883b0afa020656fae86155ff35ee0b30917c5d2
RoxaneGall <46679667+RoxaneGall@users.noreply.github.com>
2019-05-23 09:54:31 +0200

    DAO Rox

--- src/main/java/fr/insalyon/dasi/positif/dao/ConversationDAO.java
+++ src/main/java/fr/insalyon/dasi/positif/dao/ConversationDAO.java
@@ -1,7 +1,9 @@
 package fr.insalyon.dasi.positif.dao;
 
 import fr.insalyon.dasi.positif.metier.modele.Conversation;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  * Le Data Access Object de Conversation
@@ -30,6 +32,22 @@ public class ConversationDAO {
     public static Conversation obtenirConversationParId(long id) {
          EntityManager em = JpaUtil.obtenirEntityManager();
          return em.find(Conversation.class, id);
+    }
+    
+    public static List <Conversation> obtenirConvParMedium(long client_id,long idMedium) {
+         EntityManager em = JpaUtil.obtenirEntityManager();
+         try {
+            Query q = em.createQuery("SELECT c "
+                    + "FROM Conversation c "
+                    + "WHERE c.client_id= :client_id ");
+                    // + "AND c.medium_id= :idMedium");
+            q.setParameter("client_id", client_id);
+            // q.setParameter("idMedium", idMedium);
+            System.out.println("*******************************************************************************"+((q.getResultList()).get(0)));
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
     }
     
 }
